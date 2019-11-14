8c110e8f5a759d7e859c28ca57f34ba9ce0238c8
Cbazil <chadbosch08@gmail.com>
2019-11-14 16:15:34 +0200

    vue init simple done, made 2 list with different styling

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+      <ProductListOne v-bind:products="products"></ProductListOne>
+      <ProductListTwo v-bind:products="products"></ProductListTwo>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
+export default {
+  components: {
+    'ProductListOne': ProductListOne,
+    'ProductListTwo': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny Star', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+
+</style>
